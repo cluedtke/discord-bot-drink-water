88cb5212631d48d94c4a2a7d29dbaee175c8136c
Craig Luedtke <craigluedtke@gmail.com>
2023-11-20 13:54:03 -0600

    fix ref

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ resource "aws_cloudwatch_event_target" "lambda" {
 }
 
 resource "aws_cloudwatch_log_group" "logs" {
-  name = "/aws/lambda/${aws_lambda_function.api.function_name}"
+  name = "/aws/lambda/${aws_lambda_function.discord_bot.function_name}"
 
   retention_in_days = 30
 }
