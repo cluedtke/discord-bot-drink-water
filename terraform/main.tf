958691304049ac072af0025c5ff7020ffdd83158
Craig Luedtke <craigluedtke@gmail.com>
2023-11-20 11:26:41 -0600

    fix role policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,17 +17,17 @@ resource "aws_iam_role" "discord_bot" {
   name = "discord-bot-role"
 
   assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        }
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
       }
-    ]
-  }
-  EOF
+    }
+  ]
+}
+EOF
 }
